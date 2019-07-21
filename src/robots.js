06d2d12e36d44a4e915d65557a38aa19afdca03d
RTKentoo <r.t.kentoo@gmail.com>
2019-07-21 13:31:10 -0700

    Initial upload, robot cards, search bar
    Search bar uploaded without functionality.

---
+++ src/robots.js
@@ -0,0 +1,55 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Kathryn Taylor',
+	username: 'kathyrulez',
+	email: 'knicoletaylor@gmail.com'
+},
+{
+	id: 2,
+	name: 'Kent Taylor',
+	username: 'raytay',
+	email: 'r.t.kentoo@gmail.com'
+},
+{
+	id: 3,
+	name: 'Joey Tramboni',
+	username: 'joesefff',
+	email: 'joe123@gmail.com'
+},
+{
+	id: 4,
+	name: 'Itsa Mustafa',
+	username: 'I.M.aGod',
+	email: 'itsarealboi8@gmail.com'
+},
+{
+	id: 5,
+	name: 'Chaannum West',
+	username: 'Westside44',
+	email: 'detailsunknownk@msn.com'
+},
+{
+	id: 6,
+	name: 'Sufjan Stevens',
+	username: 'alwaysSadNeverHappy',
+	email: 'supersadsongs@gmail.com'
+},
+{
+	id: 7,
+	name: 'Archibald Machenzie',
+	username: 'ruffDay',
+	email: 'blueballz4lyfe@gmail.com'
+},
+{
+	id: 8,
+	name: 'Jasmine Henderson',
+	username: 'jjHeyHey',
+	email: 'jazzygreen@yahoo.com'
+},
+{
+	id: 9,
+	name: 'Robert Gladlow',
+	username: 'pencilpusher88',
+	email: 'rob@gmail.com'
+}];
